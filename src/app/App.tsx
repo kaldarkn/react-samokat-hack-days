d89138568cfc6ac3b090998bff84dbecd101318e
Artem_Book <artembugakov96@mail.ru>
2023-05-12 00:59:39 +0700

    Badge component added to app.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import NotFound from './pages/NotFound';
+import { Badge } from '@mui/material';
+import { Person } from '@mui/icons-material';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout title="React Samokat Hack Days" />}>
-        <Route index element={<h1>Home</h1>} />
+        <Route index element={
+          <>
+            <h1>Home</h1> 
+            <Badge badgeContent={3} color="success">
+              <Person color="action"/> 
+            </Badge>
+          </>
+        } />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
